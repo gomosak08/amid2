10ed5857f7daa5ae36a71c9e6f844b51ee5697d9
gomosak <gomosak@outlook.es>
2025-08-25 04:55:06 -0600

    pdf fix

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,6 +1,15 @@
 module AppointmentsHelper
-  def human_status(appointment)
-    key = appointment.status.to_s
-    I18n.t("activerecord.attributes.appointment.status.#{key}")
+  def human_status(obj)
+    key =
+      if obj.respond_to?(:status)
+        obj.status.to_s
+      else
+        obj.to_s
+      end
+
+    key = "scheduled" if key.blank?
+
+    I18n.t("activerecord.attributes.appointment.statuses.#{key}",
+           default: key.humanize)
   end
 end
